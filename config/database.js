5d2d867858cb5706b3ef6bae9f0cba81c02a4797
ad0n1sdllpc <100283529+ad0n1sdllpc@users.noreply.github.com>
2025-04-10 15:46:53 +0800

    Update Strapi config and dependencies for Supabase setup

--- config/database.js
+++ config/database.js
@@ -1,18 +1,33 @@
-    // strapi-api/config/database.js
-    module.exports = ({ env }) => ({
+module.exports = ({ env }) => {
+  const client = env("DATABASE_CLIENT");
+
+  const connections = {
+    postgres: {
       connection: {
-        client: 'postgres',
-        connection: {
-          host: env('DATABASE_HOST'),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME'),
-          user: env('DATABASE_USERNAME'),
-          password: env('DATABASE_PASSWORD'),
-          schema: env('DATABASE_SCHEMA', 'public'), // Not required
-          ssl: {
-            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-          },
+        connectionString: env("DATABASE_URL"),
+        ssl: env.bool("DATABASE_SSL", false) && {
+          rejectUnauthorized: env.bool(
+            "DATABASE_SSL_REJECT_UNAUTHORIZED",
+            true
+          ),
         },
-        debug: false,
+        schema: env("DATABASE_SCHEMA", "public"),
       },
-    });
+
+      pool: {
+        min: env.int("DATABASE_POOL_MIN"),
+        max: env.int("DATABASE_POOL_MAX"),
+      },
+    },
+  };
+
+  return {
+    connection: {
+      client,
+
+      ...connections[client],
+
+      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+    },
+  };
+};
